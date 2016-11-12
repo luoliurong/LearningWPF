5683d64ce6d20c2e29ac5a987c7b55e86eac1a59
leono <leono@DESKTOP-ANU721U>
2016-11-12 07:50:14 -0800

    remove some code behind as of now

--- LearningWPF/DDWClient/MainWindow.xaml.cs
+++ LearningWPF/DDWClient/MainWindow.xaml.cs
@@ -21,80 +21,11 @@ using Xceed.Wpf.AvalonDock;
 
 namespace DataTransform
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
-		#region TestTimer
-
-		/// <summary>
-		/// TestTimer Dependency Property
-		/// </summary>
-		public static readonly DependencyProperty TestTimerProperty =
-			DependencyProperty.Register("TestTimer", typeof(int), typeof(MainWindow),
-				new FrameworkPropertyMetadata((int)0));
-
-		/// <summary>
-		/// Gets or sets the TestTimer property.  This dependency property 
-		/// indicates a test timer that elapses evry one second (just for binding test).
-		/// </summary>
-		public int TestTimer
-		{
-			get { return (int)GetValue(TestTimerProperty); }
-			set { SetValue(TestTimerProperty, value); }
-		}
-
-		#endregion
-
-		#region FocusedElement
-
-		/// <summary>
-		/// FocusedElement Dependency Property
-		/// </summary>
-		public static readonly DependencyProperty FocusedElementProperty =
-			DependencyProperty.Register("FocusedElement", typeof(string), typeof(MainWindow),
-				new FrameworkPropertyMetadata((IInputElement)null));
-
-		/// <summary>
-		/// Gets or sets the FocusedElement property.  This dependency property 
-		/// indicates ....
-		/// </summary>
-		public string FocusedElement
-		{
-			get { return (string)GetValue(FocusedElementProperty); }
-			set { SetValue(FocusedElementProperty, value); }
-		}
-
-		#endregion
-
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			DispatcherTimer timer = new DispatcherTimer();
-			Random rnd = new Random();
-			timer.Interval = TimeSpan.FromSeconds(1.0);
-			timer.Tick += (s, e) =>
-			{
-				TestTimer++;
-
-				FocusedElement = Keyboard.FocusedElement == null ? string.Empty : Keyboard.FocusedElement.ToString();
-				//Debug.WriteLine(string.Format("ActiveContent = {0}", dockManager.ActiveContent));
-
-			};
-			timer.Start();
-
-			this.DataContext = this;
-		}
-
-		private void OnLayoutRootPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			var activeContent = ((LayoutRoot)sender).ActiveContent;
-			if (e.PropertyName == "ActiveContent")
-			{
-				System.Diagnostics.Debug.WriteLine(string.Format("ActiveContent-> {0}", activeContent));
-			}
 		}
 		private void OnDocumentPanelCreated(Xceed.Wpf.AvalonDock.Controls.LayoutDocumentPaneControl obj)
 		{
@@ -105,72 +36,56 @@ namespace DataTransform
 
 		}
 
-		private void OnLoadLayout(object sender, RoutedEventArgs e)
-		{
-			var currentContentsList = dockManager.Layout.Descendents().OfType<LayoutContent>().Where(c => c.ContentId != null).ToArray();
+		//private void OnLoadLayout(object sender, RoutedEventArgs e)
+		//{
+		//	var currentContentsList = dockManager.Layout.Descendents().OfType<LayoutContent>().Where(c => c.ContentId != null).ToArray();
 
-			string fileName = (sender as MenuItem).Header.ToString();
-			var serializer = new XmlLayoutSerializer(dockManager);
-			//serializer.LayoutSerializationCallback += (s, args) =>
-			//    {
-			//        var prevContent = currentContentsList.FirstOrDefault(c => c.ContentId == args.Model.ContentId);
-			//        if (prevContent != null)
-			//            args.Content = prevContent.Content;
-			//    };
-			using (var stream = new StreamReader(string.Format(@".\AvalonDock_{0}.config", fileName)))
-				serializer.Deserialize(stream);
-		}
+		//	string fileName = (sender as MenuItem).Header.ToString();
+		//	var serializer = new XmlLayoutSerializer(dockManager);
+		//	using (var stream = new StreamReader(string.Format(@".\AvalonDock_{0}.config", fileName)))
+		//		serializer.Deserialize(stream);
+		//}
 
-		private void AddTwoDocuments_click(object sender, RoutedEventArgs e)
-		{
-			var firstDocumentPane = dockManager.Layout.Descendents().OfType<LayoutDocumentPane>().FirstOrDefault();
-			if (firstDocumentPane != null)
-			{
-				LayoutDocument doc = new LayoutDocument();
-				doc.Title = "Test1";
-				firstDocumentPane.Children.Add(doc);
+		//private void AddTwoDocuments_click(object sender, RoutedEventArgs e)
+		//{
+		//	var firstDocumentPane = dockManager.Layout.Descendents().OfType<LayoutDocumentPane>().FirstOrDefault();
+		//	if (firstDocumentPane != null)
+		//	{
+		//		LayoutDocument doc = new LayoutDocument();
+		//		doc.Title = "Test1";
+		//		firstDocumentPane.Children.Add(doc);
 
-				LayoutDocument doc2 = new LayoutDocument();
-				doc2.Title = "Test2";
-				firstDocumentPane.Children.Add(doc2);
-			}
+		//		LayoutDocument doc2 = new LayoutDocument();
+		//		doc2.Title = "Test2";
+		//		firstDocumentPane.Children.Add(doc2);
+		//	}
+		//}
 
-			var leftAnchorGroup = dockManager.Layout.LeftSide.Children.FirstOrDefault();
-			if (leftAnchorGroup == null)
-			{
-				leftAnchorGroup = new LayoutAnchorGroup();
-				dockManager.Layout.LeftSide.Children.Add(leftAnchorGroup);
-			}
+		//private void OnShowWinformsWindow(object sender, RoutedEventArgs e)
+		//{
+		//	var winFormsWindow = dockManager.Layout.Descendents().OfType<LayoutAnchorable>().Single(a => a.ContentId == "WinFormsWindow");
+		//	if (winFormsWindow.IsHidden)
+		//		winFormsWindow.Show();
+		//	else if (winFormsWindow.IsVisible)
+		//		winFormsWindow.IsActive = true;
+		//	else
+		//		winFormsWindow.AddToLayout(dockManager, AnchorableShowStrategy.Bottom | AnchorableShowStrategy.Most);
+		//}
+		//private void OnShowToolWindow1(object sender, RoutedEventArgs e)
+		//{
+		//	var toolWindow1 = dockManager.Layout.Descendents().OfType<LayoutAnchorable>().Single(a => a.ContentId == "toolWindow1");
+		//	if (toolWindow1.IsHidden)
+		//		toolWindow1.Show();
+		//	else if (toolWindow1.IsVisible)
+		//		toolWindow1.IsActive = true;
+		//	else
+		//		toolWindow1.AddToLayout(dockManager, AnchorableShowStrategy.Bottom | AnchorableShowStrategy.Most);
+		//}
 
-			leftAnchorGroup.Children.Add(new LayoutAnchorable() { Title = "New Anchorable" });
-
-		}
-
-		private void OnShowWinformsWindow(object sender, RoutedEventArgs e)
-		{
-			var winFormsWindow = dockManager.Layout.Descendents().OfType<LayoutAnchorable>().Single(a => a.ContentId == "WinFormsWindow");
-			if (winFormsWindow.IsHidden)
-				winFormsWindow.Show();
-			else if (winFormsWindow.IsVisible)
-				winFormsWindow.IsActive = true;
-			else
-				winFormsWindow.AddToLayout(dockManager, AnchorableShowStrategy.Bottom | AnchorableShowStrategy.Most);
-		}
-		private void OnShowToolWindow1(object sender, RoutedEventArgs e)
-		{
-			var toolWindow1 = dockManager.Layout.Descendents().OfType<LayoutAnchorable>().Single(a => a.ContentId == "toolWindow1");
-			if (toolWindow1.IsHidden)
-				toolWindow1.Show();
-			else if (toolWindow1.IsVisible)
-				toolWindow1.IsActive = true;
-			else
-				toolWindow1.AddToLayout(dockManager, AnchorableShowStrategy.Bottom | AnchorableShowStrategy.Most);
-		}
-
-		private void OnToolWindow1Hiding(object sender, System.ComponentModel.CancelEventArgs e)
-		{
-			if (MessageBox.Show("Are you sure you want to hide this tool?", "AvalonDock", MessageBoxButton.YesNo) == MessageBoxResult.No)
-				e.Cancel = true;
-		}
+		//private void OnToolWindow1Hiding(object sender, System.ComponentModel.CancelEventArgs e)
+		//{
+		//	if (MessageBox.Show("Are you sure you want to hide this tool?", "AvalonDock", MessageBoxButton.YesNo) == MessageBoxResult.No)
+		//		e.Cancel = true;
+		//}
 	}
 }
