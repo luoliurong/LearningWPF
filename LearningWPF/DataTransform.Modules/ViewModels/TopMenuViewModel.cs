3e1a7ae10644c322291558307d2c62816f3eb3f4
leono <leono@DESKTOP-ANU721U>
2017-01-18 07:32:04 -0800

    add customized popup window

--- LearningWPF/DataTransform.Modules/ViewModels/TopMenuViewModel.cs
+++ LearningWPF/DataTransform.Modules/ViewModels/TopMenuViewModel.cs
@@ -15,10 +15,14 @@ namespace DataTransform.Modules.ViewModels
 		public TopMenuViewModel()
 		{
 			this.NewRepositoryRequest = new InteractionRequest<INotification>();
+			this.CustomPopupViewRequest = new InteractionRequest<INotification>();
 			this.RaiseNewRepositoryCommand = new DelegateCommand(RaiseNewRepositoryNotification);
+			this.LoadSchemaCommand = new DelegateCommand(this.RaiseCustomPopupView);
 		}
 		public InteractionRequest<INotification> NewRepositoryRequest { get; private set; }
+		public InteractionRequest<INotification> CustomPopupViewRequest { get; private set; }
 		public ICommand RaiseNewRepositoryCommand { get; private set; }
+		public ICommand LoadSchemaCommand { get; private set; }
 
 		public void RaiseNewRepositoryNotification()
 		{
@@ -30,6 +34,16 @@ namespace DataTransform.Modules.ViewModels
 		public void RaiseAboutCommandNotification()
 		{
 
+		}
+
+		private void RaiseCustomPopupView()
+		{
+			// In this case we are passing a simple notification as a parameter.
+			// The custom popup view we are using for this interaction request does not have a DataContext of its own
+			// so it will inherit the DataContext of the window, which will be this same notification.
+			//this.InteractionResultMessage = "";
+			this.CustomPopupViewRequest.Raise(
+				new Notification { Content = "Message for the CustomPopupView", Title = "Custom Popup" });
 		}
 	}
 }
--- LearningWPF/library/DataTransform.Resources/Images/ImageUris.cs
+++ LearningWPF/library/DataTransform.Resources/Images/ImageUris.cs
@@ -5,7 +5,8 @@ namespace DataTransform.Resources.Images
 {
 	public class ImageUris
 	{
-		public const string ErrorImageUri		=	@"pack://application:,,,/DataTransform.Resources;component/Images/Error.png";
-		public const string WarningImageUri		=	@"pack://application:,,,/DataTransform.Resources;component/Images/Warning.png";
+		public const string ErrorImageUri			=	@"pack://application:,,,/DataTransform.Resources;component/Images/Error.png";
+		public const string WarningImageUri			=	@"pack://application:,,,/DataTransform.Resources;component/Images/Warning.png";
+		public const string DataTransformImageUri	=	@"pack://application:,,,/DataTransform.Resources;component/Images/datatransform.ico";
 	}
 }
